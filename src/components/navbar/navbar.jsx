e7aef9ae10b17c102444f4ca49bb86fd50c43337
Madison Haradine <madisonharadine@gmail.com>
2020-02-10 15:43:07 -0500

    added home link lol

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -10,6 +10,7 @@ const NavBar = () => {
     <div className={styles.container}>
       <div><Flip><img src={logo} alt="logo" className={styles.logo}></img></Flip></div>
       <div className={styles.navlinks}>
+        <Link to='/' className={styles.link}>home</Link>
         <Link to='/projects' className={styles.link}>projects</Link>
         <Link to='/contact' className={styles.link}>connect</Link>
       </div>
