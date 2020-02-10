0929aa5974eb8d5b5b50a62a92baa20dfe7fa6f8
Madison Haradine <madisonharadine@gmail.com>
2020-02-10 14:11:09 -0500

    mobile coming soon

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,5 +1,7 @@
 import React from "react"
 import { Link } from 'gatsby'
+import Typical from 'react-typical'
+
 
 
 import styles from "./hero.module.css"
@@ -9,11 +11,18 @@ const Hero = () => {
     <>
       <div className={styles.container}>
         <div>
-          <h1 className={styles.title}>Madison Haradine</h1>
-          <h2 className={styles.subTitle}>I am a web developer experienced working with JavaScript technologies such as React and __. </h2>
-          <h2 className={styles.subTitle}>I am available for hire.<br></br><br></br>Full site coming soon.</h2>
+          <Typical
+            steps={['', 800, 'Madison Haradine', 800]}
+
+            wrapper="h1"
+            className={styles.title}
+          />
+          <h2 className={styles.subTitle}>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede. </h2>
+          <h2 className={styles.subTitle}><br></br><br></br>Full site coming soon.</h2>
         </div>
+
       </div>
+
       <div className={styles.iconBox}>
         <Link className={styles.icon}>github</Link>
         <Link className={styles.icon}>twitter</Link>
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,20 +1,21 @@
 import React from "react"
 import { Link } from "gatsby"
 import logo from "../../images/logo.png";
+import Flip from 'react-reveal'
 
 import styles from "./navbar.module.css"
 
 const NavBar = () => {
   return (
     <div className={styles.container}>
-      <div><img src={logo} alt="logo" className={styles.logo}></img></div>
+      <div><Flip><img src={logo} alt="logo" className={styles.logo}></img></Flip></div>
       <div className={styles.navlinks}>
         <Link className={styles.link}>projects</Link>
         <Link className={styles.link}>blog</Link>
         <Link className={styles.link}>resume</Link>
         <Link className={styles.link}>connect</Link>
       </div>
-    </div>
+    </div >
   )
 }
 
