161e270a6dfdefd5440bff308e000cce0949aef3
Madison Haradine <madisonharadine@gmail.com>
2020-02-11 16:51:47 -0500

    added w+j and jpc to projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,8 +4,12 @@ import styles from './styles/projects.module.css'
 
 const Projects = () => {
   return (
-    <div className={styles.container}>Projects coming soon <br></br>
-      <a href="/">Home</a></div>
+    <div className={styles.container}>Projects page coming soon <br></br>
+      <h3>Works in progress:</h3><br></br>
+      <a href="http://testingwj.netlify.com">WIND + JAMES</a><br></br>
+      <br></br>
+      <a href="http://jpc-custom-sewing.netlify.com">JPC, LLC</a><br></br> Mobile Responsive<br></br><br></br>
+      <a href="/">Back to Home</a></div>
   )
 }
 
