2abbcdfed662c30dd52791a284cfe8f626ccef3d
Madison Haradine <madisonharadine@gmail.com>
2020-02-15 14:56:31 -0500

    project cards

--- src/components/Project Card/ProjectCard.jsx
+++ src/components/Project Card/ProjectCard.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 
+
 import styles from './projectcard.module.css'
 
 const ProjectCard = (props) => {
   return (
     <div className={styles.containers}>
-      <div className={styles.card} style={{ backgroundImage: `url(${props.image})` }}>
+      <div className={styles.card} style={{ backgroundImage: `url(${props.image})`, backgroundSize: `cover` }}>
         <h3>{props.name}</h3>
         <span>{props.details}</span>
+        <a href="" className='' >
+        </a>
+
       </div>
 
     </div>
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -2,14 +2,19 @@ import React from 'react'
 import ProjectCard from '../Project Card/ProjectCard'
 
 
+
+import jpc from '../../images/jpc.png'
+import wj from '../../images/wj.png'
+
+
 import styles from './projects.module.css'
 
 const Projects = () => {
   return (
     <>
       <div className={styles.container}>
-        <ProjectCard name="Wind + James" image='' />
-        <ProjectCard name="JPC" image='' />
+        <ProjectCard name="Wind + James" image={wj} />
+        <ProjectCard name="JPC" image={jpc} />
         <ProjectCard name="Todo App" image='' />
       </div>
     </>
